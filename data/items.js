6f84a5d90f366e644b16ba76e9455f7d246d343f
myles <inbox@myles.works>
2020-02-15 22:33:58 -0500

    Update items.js

--- data/items.js
+++ data/items.js
@@ -4,8 +4,8 @@ var adulting = {
 
 	{
 		"Category" : "Header" 
-		"Title" : "Adulting logo"
-		"Description" : ""
+		"Title" : "Header"
+		"Description" : "Adulting Logo"
 		"Images" : [
 		"../img/logo.jpg", "../img/definition.jpg"
 		]
@@ -14,7 +14,7 @@ var adulting = {
 
 	{
 		"Category" : "Finance" 
-		"Title" : "Adulting logo"
+		"Title" : "Finance"
 		"Description" : ""
 		"Articles" : [ 
 		"https://www.washingtonexaminer.com/opinion/the-real-reason-millennials-are-in-such-bad-financial-shape",
@@ -28,44 +28,44 @@ var adulting = {
 
 	{
 		"Category" : "Real Estate" 
-		"Title" : "Adulting logo"
+		"Title" : "Real Estate"
 		"Description" : ""
 		"Articles" : [ 
-		"https://hbr.org/1988/05/before-you-sign-that-lease",
-		"https://www.fortunebuilders.com/real-estate-financing/",
-		"http://archive.uli.org/fallmeeting2012/wed/ULIRealEstateFinance101-JayRollins.pdf",
-		"https://millennialmoney.com/real-estate-investing-for-beginners/",
-		"https://www.youtube.com/watch?v=VXeaU0i54H0",
-		"../img/logo.jpg", "../img/definition.jpg"
+			"https://hbr.org/1988/05/before-you-sign-that-lease",
+			"https://www.fortunebuilders.com/real-estate-financing/",
+			"http://archive.uli.org/fallmeeting2012/wed/ULIRealEstateFinance101-JayRollins.pdf",
+			"https://millennialmoney.com/real-estate-investing-for-beginners/",
+			"https://www.youtube.com/watch?v=VXeaU0i54H0",
+			"../img/logo.jpg", "../img/definition.jpg"
 		]
 
 	},
 {
-		"Category" : "Stock Market" 
-		"Title" : "Adulting logo"
+		"Category" : "Finance" 
+		"Title" : "Stock Market"
 		"Description" : ""
 		"Articles" : [ 
-		"https://www.nerdwallet.com/blog/investing/stock-market-basics-everything-beginner-investors-know/",
-		"https://www.nerdwallet.com/blog/investing/avoiding-the-stock-market-may-cost-millennials-3-3-million/",
-		"https://www.investors.com/etfs-and-funds/sectors/top-stocks-making-millennials-fortune/",
-		"https://www.youtube.com/watch?v=F3QpgXBtDeo",
-		"https://www.youtube.com/watch?v=Mxu85SCT9lQ",
-		"https://www.youtube.com/watch?v=Sbp_t4guM8g",
-		"../img/logo.jpg", "../img/definition.jpg"
+			"https://www.nerdwallet.com/blog/investing/stock-market-basics-everything-beginner-investors-know/",
+			"https://www.nerdwallet.com/blog/investing/avoiding-the-stock-market-may-cost-millennials-3-3-million/",
+			"https://www.investors.com/etfs-and-funds/sectors/top-stocks-making-millennials-fortune/",
+			"https://www.youtube.com/watch?v=F3QpgXBtDeo",
+			"https://www.youtube.com/watch?v=Mxu85SCT9lQ",
+			"https://www.youtube.com/watch?v=Sbp_t4guM8g",
+			"../img/logo.jpg", "../img/definition.jpg"
 		]
 
 	},
 {
-		"Category" : "Retirement" 
-		"Title" : "Adulting logo"
+		"Category" : "Finance" 
+		"Title" : "Retirement"
 		"Description" : ""
 		"Articles" : [ 
-		"https://www.dol.gov/sites/dolgov/files/ebsa/about-ebsa/our-activities/resource-center/publications/top-10-ways-to-prepare-for-retirement.pdf",
-		"https://www.businessinsider.com/tips-make-retirement-savings-last-2019-5",
-		"https://www.youtube.com/watch?v=NREpzfGU3Bo",
-		"https://www.youtube.com/watch?v=by8mp1XMQ5E",
-		"https://www.youtube.com/watch?v=ApHRfYA1A-Y",
-		"../img/logo.jpg", "../img/definition.jpg"
+			"https://www.dol.gov/sites/dolgov/files/ebsa/about-ebsa/our-activities/resource-center/publications/top-10-ways-to-prepare-for-retirement.pdf",
+			"https://www.businessinsider.com/tips-make-retirement-savings-last-2019-5",
+			"https://www.youtube.com/watch?v=NREpzfGU3Bo",
+			"https://www.youtube.com/watch?v=by8mp1XMQ5E",
+			"https://www.youtube.com/watch?v=ApHRfYA1A-Y",
+			"../img/logo.jpg", "../img/definition.jpg"
 		]
 
 
