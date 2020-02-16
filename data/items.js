4189ff43d7e68a2b8366e174e0af916705ea35a3
myles <inbox@myles.works>
2020-02-16 00:03:43 -0500

    Update items.js

--- data/items.js
+++ data/items.js
@@ -3,32 +3,32 @@ var adulting = {
 	Items: [
 
 		{
-			"Category" : "Header" 
-			"Title" : "Header"
-			"Description" : "Adulting Logo"
+			"Category" : "Header",
+			"Title" : "Header",
+			"Description" : "Adulting Logo",
 			"Images" : [
 			"../img/logo.jpg", "../img/definition.jpg"
-			]
+			],
 
 		},
 
 		{
-			"Category" : "Finance" 
-			"Title" : "Finance"
-			"Description" : ""
+			"Category" : "Finance",
+			"Title" : "Finance",
+			"Description" : "",
 			"Articles" : [ 
 				"https://www.washingtonexaminer.com/opinion/the-real-reason-millennials-are-in-such-bad-financial-shape",
 				"https://www.businessinsider.com/money-problems-millennials-facing-student-loan-debt-homeownership-2019-5",
 				"https://www.businessinsider.com/not-all-millennials-are-financially-literate-pass-this-test-2019-4",
 				"https://gflec.org/initiatives/millennial-financial-literacy-and-fin-tech-use/",
-			]
+			],
 
 		},
 
 		{
-			"Category" : "Finance" 
-			"Title" : "Stock Market"
-			"Description" : ""
+			"Category" : "Finance",
+			"Title" : "Stock Market",
+			"Description" : "",
 			"Articles" : [ 
 				"https://www.nerdwallet.com/blog/investing/stock-market-basics-everything-beginner-investors-know/",
 				"https://www.nerdwallet.com/blog/investing/avoiding-the-stock-market-may-cost-millennials-3-3-million/",
@@ -36,13 +36,13 @@ var adulting = {
 				"https://www.youtube.com/watch?v=F3QpgXBtDeo",
 				"https://www.youtube.com/watch?v=Mxu85SCT9lQ",
 				"https://www.youtube.com/watch?v=Sbp_t4guM8g",
-			]
+			],
 
 		},
 
 		{
-			"Category" : "Finance" 
-			"Title" : "Retirement"
+			"Category" : "Finance",
+			"Title" : "Retirement",
 			"Description" : ""
 			"Articles" : [ 
 				"https://www.dol.gov/sites/dolgov/files/ebsa/about-ebsa/our-activities/resource-center/publications/top-10-ways-to-prepare-for-retirement.pdf",
@@ -50,36 +50,36 @@ var adulting = {
 				"https://www.youtube.com/watch?v=NREpzfGU3Bo",
 				"https://www.youtube.com/watch?v=by8mp1XMQ5E",
 				"https://www.youtube.com/watch?v=ApHRfYA1A-Y",
-			]
+			],
 		},
 
 
 		{
-			"Category" : "Real Estate" 
-			"Title" : "Real Estate"
-			"Description" : ""
+			"Category" : "Real Estate",
+			"Title" : "Real Estate",
+			"Description" : "",
 			"Articles" : [ 
 				"https://hbr.org/1988/05/before-you-sign-that-lease",
 				"https://www.fortunebuilders.com/real-estate-financing/",
 				"http://archive.uli.org/fallmeeting2012/wed/ULIRealEstateFinance101-JayRollins.pdf",
 				"https://millennialmoney.com/real-estate-investing-for-beginners/",
 				"https://www.youtube.com/watch?v=VXeaU0i54H0",
-			]
+			],
 
 		},
 
 		{
-			"Category" : "Real Estate" 
-			"Title" : "Selection"
-			"Description" : "Real Estate, options"
+			"Category" : "Real Estate",
+			"Title" : "Selection",
+			"Description" : "Real Estate, options",
 			"Articles" : [ 
 				"Craigslist",
 				"Zillow",
 				"Facebook",
 				"Cozy",
 				"Curbed",
-			]
+			],
 
 		},
-	]
+	]''
 }
