444fc224af2416387d7c3d75d3a91b1984e471e4
sandienunez <snunez@vt.edu>
2020-02-15 22:55:23 -0500

    Update items.js

--- data/items.js
+++ data/items.js
@@ -17,11 +17,11 @@ var adulting = {
 		"Title" : "Finance"
 		"Description" : ""
 		"Articles" : [ 
-		"https://www.washingtonexaminer.com/opinion/the-real-reason-millennials-are-in-such-bad-financial-shape",
-		"https://www.businessinsider.com/money-problems-millennials-facing-student-loan-debt-homeownership-2019-5",
-		"https://www.businessinsider.com/not-all-millennials-are-financially-literate-pass-this-test-2019-4",
-		"https://gflec.org/initiatives/millennial-financial-literacy-and-fin-tech-use/",
-		"../img/logo.jpg", "../img/definition.jpg"
+			"https://www.washingtonexaminer.com/opinion/the-real-reason-millennials-are-in-such-bad-financial-shape",
+			"https://www.businessinsider.com/money-problems-millennials-facing-student-loan-debt-homeownership-2019-5",
+			"https://www.businessinsider.com/not-all-millennials-are-financially-literate-pass-this-test-2019-4",
+			"https://gflec.org/initiatives/millennial-financial-literacy-and-fin-tech-use/",
+			"../img/logo.jpg", "../img/definition.jpg"
 		]
 
 	},
